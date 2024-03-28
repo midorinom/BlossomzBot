75708e02f620196bbf9165ebc8f3b5f0cc0b5e5e
midorinom <107839253+midorinom@users.noreply.github.com>
2024-03-28 17:44:30 +0800

    added button options for changing a guest's role to other roles. Only message responses, no functionality

---
+++ functions.py
@@ -0,0 +1,38 @@
+from interactions import ActionRow, Button, ButtonStyle
+
+def create_action_rows_horizontally(components):
+    action_rows: list[ActionRow] = [ActionRow(*components)]
+
+    return action_rows
+
+
+def create_resolve_guest_buttons(display_name):
+    member_button = Button(
+        style=ButtonStyle.SUCCESS,
+        label="Member",
+        custom_id=f"member_button_{display_name}",
+        disabled=False,
+    )
+
+    best_friend_button = Button(
+        style=ButtonStyle.PRIMARY,
+        label="Best Friend",
+        custom_id=f"best_friend_button_{display_name}",
+        disabled=False,
+    )
+
+    friend_button = Button(
+        style=ButtonStyle.DANGER,
+        label="Friend",
+        custom_id=f"friend_button_{display_name}",
+        disabled=False,
+    )
+
+    guest_button = Button(
+        style=ButtonStyle.SECONDARY,
+        label="Remain as Guest",
+        custom_id=f"guest_button_{display_name}",
+        disabled=False,
+    )
+
+    return [member_button, best_friend_button, friend_button, guest_button]
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from os import getenv
 from dotenv import load_dotenv
-from interactions import Client, Intents, listen, slash_command, SlashContext
+from interactions import Client, Intents, listen, slash_command, SlashContext, ComponentContext, component_callback
 from interactions.api.events import CommandError, MemberUpdate
+from functions import create_resolve_guest_buttons, create_action_rows_horizontally
 import traceback
+import re
 
 
 # Load Env Variables
@@ -10,6 +12,10 @@ load_dotenv()
 DISCORD_TOKEN = getenv("DISCORD_TOKEN")
 GUILD_ID = getenv("GUILD_ID")
 SCOPES = [GUILD_ID]
+
+
+# Server Variables
+guest_role = 1221723231862657056
 
 
 # Initialise Bot
@@ -32,11 +38,38 @@ async def on_command_error(event: CommandError):
 async def on_member_update(event: MemberUpdate):
     before = event.before
     after = event.after
+    blossomz_bot_channel = event.client.get_channel(1222459301201973359)
 
-    if not before.has_role(1221723231862657056) and after.has_role(1221723231862657056):
-        print(f"{event.after.display_name} has just been given the Guest role.")
-    elif before.has_role(1221723231862657056) and not after.has_role(1221723231862657056):
-        print(f"The Guest role has just been removed from {event.after.display_name}.")
+    # Resolve Guest Role
+    if after.has_role(guest_role):
+        content = f"{event.after.display_name} has the Guest role. Which role do you want to change it to?"
+        components = create_resolve_guest_buttons(event.after.display_name)
+        action_rows = create_action_rows_horizontally(components)
+
+        await blossomz_bot_channel.send(content=content, components=action_rows)
+
+
+# Component Listeners
+resolve_guest_button_regex = re.compile(r"(\w+)_button_(\w+)")
+@component_callback(resolve_guest_button_regex)
+async def resolve_guest_button_callback(ctx: ComponentContext):
+    match = resolve_guest_button_regex.match(ctx.custom_id)
+    if match:
+        chosen_option = match.group(1)
+        display_name = match.group(2)
+
+        match (chosen_option):
+            case "member":
+                await ctx.send(f"{display_name}'s role has been changed from Guest to Member.")
+            case "best_friend":
+                await ctx.send(f"{display_name}'s role has been changed from Guest to Best Friend.")
+            case "friend":
+                await ctx.send(f"{display_name}'s role has been changed from Guest to Friend.")
+            case "guest":
+                await ctx.send(f"{display_name} will continue having the Guest role.")
+            case _:
+                raise Exception(f"No option was selected for resolving {display_name}'s Guest role.")
+            
 
 # Slash Commands
 @slash_command(name="hello", description="Says Hello", scopes=SCOPES)
@@ -45,11 +78,11 @@ async def hello(ctx: SlashContext):
 
 @slash_command(name="give_guest_role", description="Gives the Guest role", scopes=SCOPES)
 async def give_guest_role(ctx: SlashContext):
-    if not ctx.member.has_role(1221723231862657056):
-        await ctx.member.add_role(role=1221723231862657056)
+    if not ctx.member.has_role(guest_role):
+        await ctx.member.add_role(role=guest_role)
         await ctx.send("You now have the guest role.")
     else:
-        await ctx.member.remove_role(role=1221723231862657056)
+        await ctx.member.remove_role(role=guest_role)
         await ctx.send("You now no longer have the guest role.")
 
 # Start Bot
