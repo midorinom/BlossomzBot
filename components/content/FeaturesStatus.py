7e4b3a96f09465e04c51c189aef04955217d7fa6
midorinom <107839253+midorinom@users.noreply.github.com>
2024-04-12 03:26:17 +0800

    capitalised F

--- components/content/FeaturesStatus.py
+++ components/content/FeaturesStatus.py
@@ -2,7 +2,7 @@ from config import config_values
 
 def generate_features_status():
     features_status = (
-        f"-- Status of BlossomzBot features --\n"
+        f"-- Status of BlossomzBot Features --\n"
         f"Managing Guests : {'Enabled' if config_values['status_managing_guests'] else 'Disabled'}\n"
     )
 
