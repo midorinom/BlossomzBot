c6be2cbb60ef0161c7634b5dad61f92c9d384f68
midorinom <107839253+midorinom@users.noreply.github.com>
2024-04-12 02:55:45 +0800

    completed functionality for /configure command

---
+++ components/select/ConfigureSelect.py
@@ -0,0 +1,21 @@
+from interactions import StringSelectMenu
+from config import config_values
+
+def generate_configure_options():
+    configure_options = [
+        f"{'Disable' if config_values['status_managing_guests'] else 'Enable'} 'Managing Guests' Feature\n",
+        "Done"
+        ]
+    
+    return configure_options
+
+def generate_configure_select_component():
+    configure_select_component = StringSelectMenu(
+        generate_configure_options(),
+        placeholder="What action would you like to take?",
+        min_values=1,
+        max_values=1,
+        custom_id="configure_select"
+        )
+    
+    return configure_select_component
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from os import getenv
 from dotenv import load_dotenv
-from interactions import Client, Intents, listen, ComponentContext, component_callback
+from interactions import Client, Intents, listen, ComponentContext, component_callback, slash_command, SlashContext
 from interactions.api.events import CommandError, MemberUpdate
-from functions import create_resolve_guest_buttons, create_action_rows_horizontally
+from config import config_values
+from functions import create_resolve_guest_buttons, create_action_rows_horizontally, convert_feature_to_config_key
+from components.select.ConfigureSelect import generate_configure_select_component
+from components.content.FeaturesStatus import generate_features_status
 import traceback
-from config import member_role, best_friend_role, friend_role, guest_role, managing_guests_channel_id
 import re
 
 
@@ -35,10 +37,10 @@ async def on_command_error(event: CommandError):
 async def on_member_update(event: MemberUpdate):
     before = event.before
     after = event.after
-    managing_guests_channel = event.client.get_channel(managing_guests_channel_id)
+    managing_guests_channel = event.client.get_channel(config_values["managing_guests_channel_id"])
 
     # Resolve Guest Role
-    if after.has_role(guest_role):
+    if config_values["status_managing_guests"] and after.has_role(config_values["guest_role"]):
         content = f"{event.after.display_name} has the Guest role. Which role do you want to change it to?"
         components = create_resolve_guest_buttons(username = event.after.username, display_name = event.after.display_name, member_id = event.after.id)
         action_rows = create_action_rows_horizontally(components)
@@ -64,49 +66,98 @@ async def resolve_guest_button_callback(ctx: ComponentContext):
         match (chosen_option):
             case "member":
                 member = ctx.guild.get_member(member_id)
-                await member.remove_role(guest_role)
+                await member.remove_role(config_values["guest_role"])
 
                 # Checking for extra roles
-                if member.has_role(friend_role):
-                    await member.remove_role(friend_role)
-                if member.has_role(best_friend_role):
-                    await member.remove_role(best_friend_role)   
+                if member.has_role(config_values["friend_role"]):
+                    await member.remove_role(config_values["friend_role"])
+                if member.has_role(config_values["best_friend_role"]):
+                    await member.remove_role(config_values["best_friend_role"])   
 
-                await member.add_role(member_role) 
+                await member.add_role(config_values["member_role"]) 
                 await ctx.edit_origin(content=f"{name} now has the Member role. The Guest role was removed.", components=[])
 
             case "best_friend":
                 member = ctx.guild.get_member(member_id)
-                await member.remove_role(guest_role)
+                await member.remove_role(config_values["guest_role"])
 
                 # Checking for extra roles
-                if member.has_role(member_role):
-                    await member.remove_role(member_role)
-                if member.has_role(friend_role):
-                    await member.remove_role(friend_role)   
+                if member.has_role(config_values["member_role"]):
+                    await member.remove_role(config_values["member_role"])
+                if member.has_role(config_values["friend_role"]):
+                    await member.remove_role(config_values["friend_role"])   
 
-                await member.add_role(best_friend_role) 
+                await member.add_role(config_values["best_friend_role"]) 
                 await ctx.edit_origin(content=f"{name} now has the Best Friend role. The Guest role was removed.", components=[])
 
             case "friend":
                 member = ctx.guild.get_member(member_id)
-                await member.remove_role(guest_role)
+                await member.remove_role(config_values["guest_role"])
 
                 # Checking for extra roles
-                if member.has_role(member_role):
-                    await member.remove_role(member_role)
-                if member.has_role(best_friend_role):
-                    await member.remove_role(best_friend_role)   
+                if member.has_role(config_values["member_role"]):
+                    await member.remove_role(config_values["member_role"])
+                if member.has_role(config_values["best_friend_role"]):
+                    await member.remove_role(config_values["best_friend_role"])   
 
-                await member.add_role(friend_role) 
+                await member.add_role(config_values["friend_role"]) 
                 await ctx.edit_origin(content=f"{name} now has the Friend role. The Guest role was removed.", components=[])
 
             case "guest":
+                member = ctx.guild.get_member(member_id)
+
+                # Checking for extra roles
+                if member.has_role(config_values["member_role"]):
+                    await member.remove_role(config_values["member_role"])
+                if member.has_role(config_values["best_friend_role"]):
+                    await member.remove_role(config_values["best_friend_role"])
+                if member.has_role(config_values["friend_role"]):
+                    await member.remove_role(config_values["friend_role"])    
+
                 await ctx.edit_origin(content=f"{name} will continue having the Guest role.", components=[])
 
             case _:
                 raise Exception(f"No option was selected for resolving {name}'s Guest role.")
-            
+        
+@component_callback("configure_select")
+async def configure_select_callback(ctx: ComponentContext):
+    selected_option = ctx.values[0]
+    if selected_option == "Done":
+        await ctx.defer(edit_origin=True)
+        await ctx.delete(ctx.message)
+    else:
+        enable_feature_regex = re.compile(r"Enable '([^']+)' Feature")
+        match_enable_feature = enable_feature_regex.match(selected_option)
+        if match_enable_feature:
+            feature = match_enable_feature.group(1)
+            config_key = convert_feature_to_config_key(feature)
+            config_values[config_key] = True
+
+            await ctx.edit_origin(content=f"The '{feature}' feature is now enabled.\n\n{generate_features_status()}", components=[])
+        
+        else:
+            disable_feature_regex = re.compile(r"Disable '([^']+)' Feature")
+            match_disable_feature = disable_feature_regex.match(selected_option)
+            if match_disable_feature:
+                feature = match_disable_feature.group(1)
+                config_key = convert_feature_to_config_key(feature)
+                config_values[config_key] = False
+
+                await ctx.edit_origin(content=f"The '{feature}' feature is now disabled.\n\n{generate_features_status()}", components=[])
+
+
+# Slash Commands
+@slash_command(name="configure", description="Shows the status of each BlossomzBot feature and allows for enabling/disabling them", scopes=SCOPES)
+async def configure(ctx: SlashContext):
+    if not ctx.member.has_role(config_values["leader_role"]) and not ctx.member.has_role(config_values["officer_role"]):
+        await ctx.send("You do not have permission to use this command.", ephemeral=True)
+    else:
+        blossomz_bot_channel = ctx.guild.get_channel(config_values["blossomz_bot_channel_id"])
+        
+        await ctx.send("Loading...", ephemeral=True)
+        await ctx.delete()
+        await blossomz_bot_channel.send(content=generate_features_status(), components=generate_configure_select_component())
+    
 
 # Start Bot
 bot.start()
