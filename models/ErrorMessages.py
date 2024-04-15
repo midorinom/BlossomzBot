15ea5f8695292def90ac86ece2d5cea97420fd3d
midorinom <107839253+midorinom@users.noreply.github.com>
2024-04-15 18:57:12 +0800

    refactoring to models folder

--- models/ErrorMessages.py
+++ models/ErrorMessages.py
@@ -1,3 +1,4 @@
 error_messages = {
     "01": "There was a database-related error when checking whether the new guest already exists in the server.",
+    "02": "There was a api-related error when trying to create a new entry in the spreadsheet.",
 }
--- models/FeaturesStatus.py
+++ models/FeaturesStatus.py
@@ -4,6 +4,6 @@ def generate_features_status():
     features_status = (
         f"-- Status of BlossomzBot Features --\n"
         f"Managing Guests : {'Enabled' if config_values['status_managing_guests'] else 'Disabled'}\n"
-    )
+        )
 
     return features_status
